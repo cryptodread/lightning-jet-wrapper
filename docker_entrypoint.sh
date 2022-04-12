68020df1d58f300e02432c714f03ba48a7cbde0c
Dread <dread@pop-os.localdomain>
2022-04-11 21:44:40 -0400

    works™

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -5,6 +5,7 @@ echo "Balance of Satoshis - Version: "
 ./bos --version
 mkdir -p /root/.bos/embassy
 chmod -R a+x /root/.bos
+
 mv ../credentials.json /root/.bos/embassy/credentials.json
 chmod a+x /root/.bos/embassy/credentials.json
 
